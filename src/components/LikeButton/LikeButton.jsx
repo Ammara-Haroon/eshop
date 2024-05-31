ddc1ed75b116b1b9f8d6dac7a3dc034e850cff87
Ammara-Haroon <160440578+Ammara-Haroon@users.noreply.github.com>
2024-05-31 16:38:00 +1000

    changed heart emoji to smaller size for smaller screens

--- src/components/LikeButton/LikeButton.jsx
+++ src/components/LikeButton/LikeButton.jsx
@@ -1,10 +1,10 @@
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 const LikeButton = ({ isLiked, setLikedStatus, className }) => {
   return (
     <button onClick={setLikedStatus} className={className}>
-      <small>
-        {isLiked ? "♥" : "♡"}
-      </small>
+      <small>{isLiked ? "♥" : "♡"}</small>
     </button>
   );
 };
